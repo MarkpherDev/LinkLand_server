1ac601bb492fdda85e9b0fe08439ceadc6901914
MartinPC <MartinChrisPC@hotmail.com>
2024-05-25 12:08:23 -0500

    ADD register and login routes

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { UserController } from "../controllers/user.controller";
 
 const router: Router = Router();
 
-router.post("/register");
-router.post("/login");
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 export default router;
